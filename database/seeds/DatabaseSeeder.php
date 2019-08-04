e53f5de95d4e5f2679b5c7a4b3e32b97921764c0
dirkSTEEL <ph8360@gmail.com>
2019-08-04 21:24:07 +1000

    Database Seeder
    Added JSON files to storage
    Set up seeder to pull data from JSON files and import to DB

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Project;
+use App\Story;
+use App\ProjectTask;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,40 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // Seed projects table
+        $json = file_get_contents( storage_path("data/projects.json") );
+        $projects_data = json_decode($json);
+        foreach ($projects_data as $obj) {
+            Project::create(array(
+                'id'    => $obj->id,
+                'name'  => $obj->name,
+            ));
+        }
+
+        // Seed stories table
+        $json = file_get_contents( storage_path("data/stories.json") );
+        $stories_data = json_decode($json);
+        foreach ($stories_data as $obj) {
+            Story::create(array(
+                'id'                => $obj->id,
+                'name'              => $obj->name,
+                'take_days'         => $obj->take_days,
+                'daily_tasks_list'  => json_encode($obj->daily_tasks_list),
+            ));
+        }
+
+        // Seed project_tasks table
+        $json = file_get_contents( storage_path("data/project_tasks.json") );
+        $project_tasks_data = json_decode($json);
+        foreach ($project_tasks_data as $obj) {
+            ProjectTask::create(array(
+                'id'            => $obj->id,
+                'project_id'    => $obj->project_id,
+                'absolute_day'  => $obj->absolute_day,
+                'name'          => $obj->name,
+                'story_id'      => $obj->story_id,
+            ));
+        }
     }
 }
